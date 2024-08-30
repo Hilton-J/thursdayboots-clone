b0b3b05823f26ebd53e8b1367d8c9d20e0dc76f3
Hilton-J <lhmajola@outlook.com>
2024-08-30 21:25:37 +0200

    Small changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
 
       <div className="flex-1 flex text-white  ">
         <div className="text-center w-full h-full md:w-[400px] space-y-9 flex flex-col mt-8 md:justify-center  md:ml-10">
-          <h1 className="font-play text-3xl md:text-5xl font-bold">Highest Quality. <br /> Honest Prices.</h1>
+          <h1 className="font-play text-5xl font-bold">Highest Quality. <br /> Honest Prices.</h1>
           <h2>Handcrafted With Integrity</h2>
           <div className=' flex-1 flex  space-x-2 md:flex-none md:items-start items-end justify-center pb-32'>
             <Button text={'SHOP MEN'} style={'bg-white hover:bg-[#d8d8d8] text-black py-[14px] px-3 text-sm min-w-[175px]'} />
--- src/components/TheEden.jsx
+++ src/components/TheEden.jsx
@@ -6,7 +6,7 @@ const TheEden = () => {
 
       <div className="flex-1 flex text-white justify-end mr-10 ">
         <div className="text-center w-full h-full md:w-[400px] space-y-9 flex flex-col md:justify-center  ">
-          <h1 className="font-play text-3xl md:text-5xl font-bold">The Eden</h1>
+          <h1 className="font-play text-5xl font-bold mt-6">The Eden</h1>
           <h2>A Sophisticated Slip-On Loafer,<br />Now Availble In 5 Colors</h2>
           <div className=' flex-1 flex md:flex-none md:items-start items-end justify-center pb-32'>
             <Button text={'SHOP NOW'} style={'bg-white hover:bg-[#d8d8d8] text-black py-[14px] px-3 text-sm min-w-[175px]'} />
